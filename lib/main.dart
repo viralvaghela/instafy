86cb30eda7bb98ab93df7104db53d6ade3f75998
viralvaghela <viralvaghela7109@gmail.com>
2021-03-18 21:07:29 +0530

    splash screen added

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main()=>runApp(MyApp());
+import 'screens/splashs_screen.dart';
 
+void main()=>runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+    showSemanticsDebugger: false,
       home: Scaffold(
         
+        
+        body: SplashScreen(),
       ),
     );
+
   }
+
 }
