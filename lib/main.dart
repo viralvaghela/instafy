1f53732796a46c15b2ef0c54c7559af9ea1f8ff3
viralvaghela <viralvaghela7109@gmail.com>
2021-03-22 20:41:15 +0530

    firestore msgs added

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    showSemanticsDebugger: false,
+    debugShowCheckedModeBanner: false,
       home: Scaffold(
         
         
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class HomeScreen extends StatelessWidget
-{
+class HomeScreen extends StatelessWidget {
+  int i = 0;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-
+    return Scaffold(
+      body: StreamBuilder(
+        stream: Firestore.instance
+            .collection("/chats/5JTmBjOBaEDFOI1xA4rt/messages/")
+            .snapshots(),
+        builder: (context, streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          final documents = streamSnapshot.data.documents;
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (context, index) => Container(
+              child: Text(documents[index]['text']),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/5JTmBjOBaEDFOI1xA4rt/messages')
+              .add({"text": "This is message $i"});
+          i++;
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
